75b2f15db2f49bb259daea0f1009ab55c13e2dd7
Karley Petracca <karleypetracca@gmail.com>
2020-03-25 03:49:09 -0400

    added class ternary for flipping

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFlipped: false
+        };
+    }
+
+    clickHandler() {
+        this.setState({ isFlipped: !this.state.isFlipped });
+    }
+
     render() {
+        let memoryCardInnerClass = this.state.isFlipped ? "memoryCardInner flipped" : "memoryCardInner";
+        
         return (
-            <div className="MemoryCard">
-                <div className="MemoryCardInner">
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
                     <div className="MemoryCardFront">
                         ∆
                     </div>
